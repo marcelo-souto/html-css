da536d3e623bda6d763a8e95377e7f6b59b9c020
Marcelo Souto <100978478+marcelo-souto@users.noreply.github.com>
2022-03-25 23:29:59 -0300

    Update project menu

--- projetos/projeto-menu-lateral/script.js
+++ projetos/projeto-menu-lateral/script.js
@@ -1,15 +1,21 @@
 const btnopen = document.querySelector("#btnopen");
 const btnclose = document.querySelector("#btnclose");
 const nav = document.querySelector("#nav");
+const overlay = document.getElementById('overlay');
 
 function abrirmenu() {
   nav.classList.add("active");
+  overlay.style.backgroundColor = '#00000050';
+  overlay.style.zIndex = '1';
 }
 
 function fecharmenu() {
   nav.classList.remove("active");
+  overlay.style.backgroundColor = 'transparent';
+  overlay.style.zIndex = '-1';
 }
 
+overlay.addEventListener('click', fecharmenu)
 btnopen.addEventListener("click", abrirmenu);
 btnclose.addEventListener("click", fecharmenu);
 
