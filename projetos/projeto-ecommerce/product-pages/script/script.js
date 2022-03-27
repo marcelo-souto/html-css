47234b9f6e44ad77545b97d986f92320936f6f1e
Marcelo Souto <100978478+marcelo-souto@users.noreply.github.com>
2022-03-27 15:04:04 -0300

    Update Projeto e-commerce

---
+++ projetos/projeto-ecommerce/product-pages/script/script.js
@@ -0,0 +1,7 @@
+// function mostrar(imgsrc) {
+//     document.getElementById('product-principal').setAttribute('src', imgsrc)
+//  } 
+
+function mostrar(imgsrc) {
+    document.getElementById('product-principal').style.backgroundImage = 'url'+'("../'+imgsrc+'")'
+ }
